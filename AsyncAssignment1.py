af02a02014e4c5e7a22e0b4d128438093ec6d6dc
teerthsrivastava2025-dot <teerth.srivastava.2025@sse.ac.in>
2026-01-04 01:48:29 +0530

    AsyncAssignment1.py

--- AsyncAssignment1.py
+++ AsyncAssignment1.py
@@ -1,102 +1,36 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 11,
-   "id": "33c8a704-d8d6-4001-958b-f0393492ede1",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "**Price Elasticity of Demand & Supply of different products**\n",
-      "Available Products:\n",
-      "1 . Rice\n",
-      "2 . Milk\n",
-      "3 . Petrol\n",
-      "4 . Books\n"
-     ]
-    },
-    {
-     "name": "stdin",
-     "output_type": "stream",
-     "text": [
-      "Select a product number:  4\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Product Selected: Books\n",
-      "Original Price: 200\n",
-      "New Price: 205\n",
-      "Price Elasticity of Demand: -23.999999999999996\n",
-      "Price Elasticity of Supply: 20.0\n"
-     ]
-    }
-   ],
-   "source": [
-    "print('**Price Elasticity of Demand & Supply of different products**')\n",
-    "products = [\"Rice\", \"Milk\", \"Petrol\", \"Books\"]\n",
-    "\n",
-    "prices = [40, 30, 100, 200]\n",
-    "quantities_demanded = [500, 300, 150, 100]\n",
-    "quantities_supplied = [450, 280, 170, 120]\n",
-    "\n",
-    "demand_change_rate = [-0.2, -0.3, -0.05, -0.6]\n",
-    "supply_change_rate = [0.25, 0.3, 0.1, 0.5]\n",
-    "\n",
-    "print(\"Available Products:\")\n",
-    "i = 0\n",
-    "while i < len(products):\n",
-    "    print(i + 1, \".\", products[i])\n",
-    "    i = i + 1\n",
-    "\n",
-    "choice = int(input(\"Select a product number: \"))\n",
-    "index = choice - 1\n",
-    "\n",
-    "price = prices[index]\n",
-    "qd = quantities_demanded[index]\n",
-    "qs = quantities_supplied[index]\n",
-    "\n",
-    "price_change = 5\n",
-    "new_price = price + price_change\n",
-    "\n",
-    "new_qd = qd + (qd * demand_change_rate[index])\n",
-    "new_qs = qs + (qs * supply_change_rate[index])\n",
-    "\n",
-    "ped = ((new_qd - qd) / qd) / ((new_price - price) / price)\n",
-    "pes = ((new_qs - qs) / qs) / ((new_price - price) / price)\n",
-    "\n",
-    "print(\"Product Selected:\", products[index])\n",
-    "print(\"Original Price:\", price)\n",
-    "print(\"New Price:\", new_price)\n",
-    "print(\"Price Elasticity of Demand:\", ped)\n",
-    "print(\"Price Elasticity of Supply:\", pes)\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python [conda env:base] *",
-   "language": "python",
-   "name": "conda-base-py"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.13.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+products = ["Rice", "Milk", "Petrol", "Books"]
+
+prices = [40, 30, 100, 200]
+quantities_demanded = [500, 300, 150, 100]
+quantities_supplied = [450, 280, 170, 120]
+
+demand_change_rate = [-0.2, -0.3, -0.05, -0.6]
+supply_change_rate = [0.25, 0.3, 0.1, 0.5]
+
+print("Available Products:")
+i = 0
+while i < len(products):
+    print(i + 1, ".", products[i])
+    i = i + 1
+
+choice = int(input("Select a product number: "))
+index = choice - 1
+
+price = prices[index]
+qd = quantities_demanded[index]
+qs = quantities_supplied[index]
+
+price_change = 5
+new_price = price + price_change
+
+new_qd = qd + (qd * demand_change_rate[index])
+new_qs = qs + (qs * supply_change_rate[index])
+
+ped = ((new_qd - qd) / qd) / ((new_price - price) / price)
+pes = ((new_qs - qs) / qs) / ((new_price - price) / price)
+
+print("Product Selected:", products[index])
+print("Original Price:", price)
+print("New Price:", new_price)
+print("Price Elasticity of Demand:", ped)
+print("Price Elasticity of Supply:", pes)
